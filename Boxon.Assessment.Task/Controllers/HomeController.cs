dae071a5bc16acae329d47d036873b05b8b426e9
numankhan12 <numan8289@gmail.com>
2024-06-20 02:36:09 +0400

    initial commit

---
+++ Boxon.Assessment.Task/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Boxon.Assessment.Task.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Boxon.Assessment.Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult UserInfoForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UserInfoForm(UserRecord model)
+        {
+            return View();
+        }
+   
+    }
+}
---
+++ Boxon.Assessment.Task/Models/UserInfoMode.cs
@@ -0,0 +1,15 @@
+﻿namespace Boxon.Assessment.Task.Models
+{
+    public class UserRecord
+    {
+        public List<UserInfoMode> UserInfoMode { get; set; }
+    }
+    public class UserInfoMode
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Language { get; set; }
+        public string ArticalId { get; set; }
+    }
+}
